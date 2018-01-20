52eea5b5c466074fe6a322a8db8db27738e0672b
Maximilian Vallo <maximilian.vallo@gmail.com>
2018-01-20 20:29:52 +0100

    typo port

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 
-const app = express ()
+const app = express()
 
-app.set(port, (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000))
 
 // Allows us to process the data
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Routes
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send("Hi I am a chatbot")
 })
 
 // Facebook
 
 app.get('/webhook/', function(req, res) {
-        if (req.query['hub.verify_token'] === "maxvallo") {
-            res.send(req.query['hub.challenge'])
-        }
-        res.send("Wrong token")
+    if (req.query['hub.verify_token'] === "maxvallo") {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send("Wrong token")
 })
 
-app.listen(app.get.('port'), function() {
+app.listen(app.get('port'), function() {
     console.log("running: port")
 })
